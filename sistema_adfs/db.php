0f953992c9edce7c547d709e2066041136a073cb
Fatiunis <fapalrz@unis.edu.gt>
2026-02-02 15:03:16 -0600

    subiendo . después de uat para regresar

--- sistema_adfs/db.php
+++ sistema_adfs/db.php
@@ -1,12 +1,11 @@
 <?php
-$host = getenv("DB_HOST");
+$host = getenv("DB_HOST") ?: 'mysql';
 $port = getenv("DB_PORT");
 $database = getenv("DB_NAME");
 $username = getenv("DB_USER");
 $password = getenv("DB_PASS");
 
-$conn = new mysqli($host, $username, $password, $database, $port);
-
+$conn = new mysqli($host, $username, $password, $database, 3306);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
